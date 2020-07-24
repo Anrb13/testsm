0891d81caf1c5b800a4900faf4734997e4d8f144
Anar Bayramov <abayramov@sportmaster.ru>
2020-07-24 20:50:38 +0300

    'start'

---
+++ config.js
@@ -0,0 +1,9 @@
+const baseUrl = (noscript) => {
+    const param = noscript ? '?noscript=1' : '?noscript=0';
+    return `https://www.sportmaster.ru${param}`
+}
+
+
+module.exports = {
+    baseUrl,
+};
